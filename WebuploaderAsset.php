b58d72c88a0c83c650a42328ce2b5d639fbc225b
xjflyttp <xjflyttp@gmail.com>
2017-11-30 11:28:57 +0800

    fix basePath

--- WebuploaderAsset.php
+++ WebuploaderAsset.php
@@ -12,7 +12,6 @@ use yii\web\AssetBundle;
 class WebuploaderAsset extends AssetBundle {
 
     public $sourcePath = '@bower/fex-webuploader/dist';
-    public $basePath = '@webroot/assets';
     public $css = ['webuploader.css'];
     public $js = ['webuploader.js'];
     public $depends = ['yii\web\JqueryAsset'];
--- WebuploaderCustomAsset.php
+++ WebuploaderCustomAsset.php
@@ -12,7 +12,6 @@ use yii\web\AssetBundle;
 class WebuploaderCustomAsset extends AssetBundle {
 
     public $sourcePath = '@bower/fex-webuploader/dist';
-    public $basePath = '@webroot/assets';
     public $css = ['webuploader.css'];
     public $js = ['webuploader.custom.js'];
     public $depends = ['yii\web\JqueryAsset'];
